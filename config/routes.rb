b3cad043bab0b092837ec804398104eea5bce3c3
junolouise <katierodgers95@gmail.com>
2020-05-01 12:08:55 +0100

    feat: user signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  
+  root "movies#index"
+  
   resources :users
   resources :movies do
     resources :reviews
   end
 
-  root "movies#index"
+  get "signup" => "users#new"
+  
 end
