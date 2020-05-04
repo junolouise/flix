81d7a1ade59bb676036af0a161322fdfc21eeb40
junolouise <katierodgers95@gmail.com>
2020-05-04 11:23:14 +0100

    feat: user signup added

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
     resources :reviews
   end
 
+  resource :session, only: [:new, :create, :destroy]
+
+  get "signin" => "sessions#new"
+
   get "signup" => "users#new"
   
 end
