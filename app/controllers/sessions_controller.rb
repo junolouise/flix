349ebaa8aa7116fa61414a19a9c95bb503a8ace5
junolouise <katierodgers95@gmail.com>
2020-05-04 12:27:33 +0100

    feat: sign in complete

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,14 @@ class SessionsController < ApplicationController
     end
 
     def create
-        fail
+        user = User.find_by(email: params[:email])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect_to user, notice: "Welcome back, #{user.name}!"
+        else
+            flash.now[:alert] = "Incorrect email/ password combination."
+            render :new
+        end
     end
 
     def destroy
