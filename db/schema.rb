c773feae97acf95e21000d1d8df29344192b3b8e
junolouise <katierodgers95@gmail.com>
2020-05-05 10:29:02 +0100

    feat: admin column added to users db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_01_134907) do
+ActiveRecord::Schema.define(version: 2020_05_05_092637) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2020_05_01_134907) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
+    t.boolean "admin", default: false
   end
 
   add_foreign_key "reviews", "movies"
