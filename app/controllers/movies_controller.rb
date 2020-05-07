2bba1ea41fd1aa1cabd51732e894f8c72ceb5981
junolouise <katierodgers95@gmail.com>
2020-05-07 13:18:45 +0100

    feat: unfavorite button working

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,60 +1,58 @@
 class MoviesController < ApplicationController
 
     before_action :require_signin, except: [:index, :show]
-
     before_action :require_admin, except: [:index, :show]
-
+  
     def index
-        @movies = Movie.released
+      @movies = Movie.released
     end
-
+  
     def show
-        @movie = Movie.find(params[:id])
-        @review = Review.new
-        @fans = @movie.fans
-        if current_user
-            @favorite = current_user.favorites.find_by(movie_id: @movie.id)
-        end
+      @movie = Movie.find(params[:id])
+      @fans = @movie.fans
+      if current_user
+        @favorite = current_user.favorites.find_by(movie_id: @movie.id)
+      end
     end
-
+  
     def edit
-        @movie = Movie.find(params[:id])
+      @movie = Movie.find(params[:id])
     end
-
+  
     def update
-        @movie = Movie.find(params[:id])
-        if @movie.update(movie_params)
-            redirect_to @movie, notice: "Movie successfully updated!"
-        else
-            render :edit
-        end
+      @movie = Movie.find(params[:id])
+      if @movie.update(movie_params)
+        redirect_to @movie, notice: "Movie successfully updated!"
+      else
+        render :edit
+      end
     end
-
+  
     def new
-        @movie = Movie.new
+      @movie = Movie.new
     end
-
+  
     def create
-        @movie = Movie.new(movie_params)
-        if @movie.save
-            redirect_to @movie, notice: "Movie successfully created!"
-        else
-            render :new
-        end
+      @movie = Movie.new(movie_params)
+      if @movie.save
+        redirect_to @movie, notice: "Movie successfully created!"
+      else
+        render :new
+      end
     end
-
+  
     def destroy
-        @movie = Movie.find(params[:id])
-        @movie.destroy
-        redirect_to movies_url, alert: "Movie has been deleted!"
+      @movie = Movie.find(params[:id])
+      @movie.destroy
+      redirect_to movies_url, alert: "Movie successfully deleted!"
     end
-
-
-    private
-
+  
+  private
+  
     def movie_params
-        params.require(:movie).
-            permit(:title, :description, :rating, :released_on, :total_gross, :director, :image_file_name, :duration)
+      params.require(:movie).
+        permit(:title, :description, :rating, :released_on, :total_gross,
+               :director, :duration, :image_file_name)
     end
-
-end
+  end
+  
