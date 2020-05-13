30e9cad1905ee99e80e7c5e068f831caddc62268
junolouise <katierodgers95@gmail.com>
2020-05-13 11:51:27 +0100

    feat: active storage used for images and validations added

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -8,6 +8,8 @@ class Movie < ApplicationRecord
 
   before_save :set_slug
 
+  has_one_attached :main_image
+
   has_many :reviews, -> { order(created_at: :desc) }, dependent: :destroy
 
   has_many :favorites, dependent: :destroy
@@ -26,14 +28,11 @@ class Movie < ApplicationRecord
   
     validates :total_gross, numericality: { greater_than_or_equal_to: 0 }
   
-    validates :image_file_name, format: {
-      with: /\w+\.(jpg|png)\z/i,
-      message: "must be a JPG or PNG image"
-    }
-  
     RATINGS = %w(G PG PG-13 R NC-17 12 12A 18)
 
     validates :rating, inclusion: { in: RATINGS }
+
+    validate :acceptable_image
   
     def flop?
       total_gross.blank? || total_gross < 225_000_000
@@ -55,5 +54,18 @@ class Movie < ApplicationRecord
 
     def set_slug
       self.slug = title.parameterize
+    end
+
+    def acceptable_image
+      return unless main_image.attached?
+    
+      unless main_image.blob.byte_size <= 1.megabyte
+        errors.add(:main_image, "is too big")
+      end
+    
+      acceptable_types = ["image/jpeg", "image/png"]
+      unless acceptable_types.include?(main_image.content_type)
+        errors.add(:main_image, "must be a JPEG or PNG")
+      end
     end
 end
